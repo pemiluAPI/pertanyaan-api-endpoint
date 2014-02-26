941e0c52f7c3122cae6fa0af7c39fba865d02b33
yobi.setiawan <yobi_bina_s@yahoo.co.id>
2014-02-26 10:45:33 +0700

    fix seacrh by multiple tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,7 +5,7 @@ class Question < ActiveRecord::Base
   def self.find_all(params = Hash.new())
     questions= Array.new
     search = ["reference_law LIKE ? or excerpt_law LIKE ?", "%#{params[:law]}%", "%#{params[:law].gsub('_', ' ')}%"] if !params[:law].blank?
-    conditions = ((params[:tags].include?(',')) ? ["tags.tag in (?)", params[:tags].split(',').gsub('_', ' ')] : ["tags.tag = ?", "#{params[:tags].gsub('_', ' ')}"]) if !params[:tags].blank?
+    conditions = ((params[:tags].include?(',')) ? ["tags.tag in (?)", params[:tags].gsub('_', ' ').split(',')] : ["tags.tag = ?", "#{params[:tags].gsub('_', ' ')}"]) if !params[:tags].blank?
     Question.includes(:tags).where(conditions).where(search).limit(params[:limit]).offset(params[:offset]).references(:tags).each do |field|
       questions << field.details
     end
